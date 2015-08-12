22c4da4d0ec064beaeba012efc7606a4188167ea
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-08-12 16:08:35 +0200

    convert Spectrometer -> VEGAS

--- observing_scripts/H2CO_2cm_KUSetup_GC.py
+++ observing_scripts/H2CO_2cm_KUSetup_GC.py
@@ -10,26 +10,27 @@
 receiver  = 'Rcvr12_18'               # select Ku-band receiver
 beam      = 'B12'                     # use two beams
 obstype   = 'Spectroscopy'
-backend   = 'Spectrometer'
-nwin      = 4                         # four spectral windows
-restfreq  = 14488.479,12178.597,13778.804,13165.96 # H2CO, CH3OH, H213CO, H2C18O; max separation 2310 MHz
-deltafreq = 0,0,0,0                   # DO NOT MENTION IF3FREQ IN SETUP!
-bandwidth = 50.0                      # MHz Moderate-resolution mode (0.25 km/s)
+backend   = 'VEGAS'
 swmode    = "tp"                      # set switching scheme (tp(total power with cal), tp_nocal, sp(switched power with cal), sp_nocal )
 swtype    = "none"                    # for frequency switching; not used for tp mode
 swper     = 1.0                       # one second cycle for switching
 swfreq    = 0.0, 0.0                  # for freq switching
-tint      = 2.0                       # integration time (for 4 quadrants, 1.2-40 sec.  Important to avoid smearing)
+tint      = 0.5                       # integration time (for 4 quadrants, 1.2-40 sec.  Important to avoid smearing)
 vlow      = 0
 vhigh     = 0
 vframe    = "lsrk"                    # LSR - kinematic is the "normal" definition (don't use dynamic)
 vdef      = "Radio"                   # radio (optical is also acceptable, but not the norm for Galactic observations)
 noisecal  = "lo"
-pol       = "Circular"
-nchan     = "high"                     # 4096 channels over 2 beams and 4 windows
-        # spectrometer guide says 12.5 MHz = 237 km/s bandwidth = 8192 channels,
-        # 50 MHz = 1035 km/s BW
-        # 4096 channels, 12.2 KHz = 0.253 km/s
-        # numsamplers=8, nwin=4, chanwidth = 1.526 KHz -> resolution = .06 km/s (2 channels)... nchan = high
-spect.levels = 9                      # nine level sampling
-#iftarget = 0.25                       # IF target is now always 1
+pol       = "Circular"  # should this be linear?
+nchan     = "high"
+vegas.vpol='self'
+restfreq = [ {"restfreq": 14488.479, 'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 14511.,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 14465.,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 13778.80,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 13801.,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 13756.,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 14151.61,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 14105.61,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+             {"restfreq": 12625.,    'bandwidth': 1250., "res":92.0, "delatfreq": 0},
+           ]
--- observing_scripts/H2CO_6cm_CSetup.py
+++ observing_scripts/H2CO_6cm_CSetup.py
@@ -1,39 +1,15 @@
 # Ku-band H2CO 2 cm (and friends)
 # keywords defined at www.gb.nrao.edu/~fghigo/gbtdoc/config/configparams_rev1.doc
-#
-# CONFIGURATION:
-# Bandwidth Polarization Level Number of Number of Lags - Approximate Resolution
-#   (MHz) Cross-Products Sampling Spectral Beams Low Medium High
-# BW Pol Lev  Windows Beams Channels / resolution
-# 50 No  9    4       2     4096 - 12.2070 kHz 4096 - 12.2070 kHz 4096 - 12.2070 kHz
 
 receiver  = 'Rcvr4_6'               # select C-band receiver
 beam      = 'B1'                     # use one beam
 obstype   = 'Spectroscopy'
-backend   = 'Spectrometer'
-nwin      = 8                         # four spectral windows
-# Maximum instantaneous IF bandwidth is 3500 MHz
-# these frequencies were used for the pilot restfreq  = 14488.479,14131.5,14693.0,15284.6  # the line freqs (add more lines!)
-# frequencies used in "obs_session1_innergalaxy": restfreq  = 14488.479,13778.804,13165.96,14693.0  # the line freqs H2CO, H2_13CO, H2_C18O, and H76a
-# used for SESSION 2, outergal, August 15 2010: restfreq  = 14488.479,13026.06,13595.49,14825.79  # the line freqs H2CO, NaCl, H78a, and H2CN
-# used for 2 scans in SESSION 3 restfreq  = 14488.479,13026.06,14128.61,14825.79  # the line freqs H2CO, NaCl, H77a, and H2CN
-# used for most of session 3, 4 restfreq  = 14488.479,13036.06,14128.61,14825.79  # the line freqs H2CO, NaCl + SO, H77a, and H2CN
-# We will also use the GBT C-band receiver with the GBT spectrometer. We will
-# use 8 spectral windows with 50 MHz (3000 km s􀀀1) bandwidth, 12.2070 kHz
-# channel width (0.75 km s􀀀1), no polarization cross- products, and 9-level
-# sampling. Our windows will target the following lines (frequencies are
-# specied in GHz): 4.8296594 (H2CO 110􀀀111), 4.593089 (H2 13CO 110􀀀111), 5.50
-# (H106 and OH), 5.20 (H107 and { 3 { CH2NH), 5.008922 (H109), 4.63
-# (H112 and OH), 4.874157 (H110), and 4.497776 (H113 and HCN)
-# # 
-restfreq  = 4829.6594, 4593.089, 5500.0, 5200.0, 5008.992, 4630.0, 4874.157, 4497.776
-deltafreq = 0,0,0,0,0,0,0,0           # DO NOT MENTION IF3FREQ IN SETUP!
-bandwidth = 50.0                      # MHz Moderate-resolution mode (0.75 km/s) -> 12 kHz channels
+backend   = 'VEGAS'
 swmode    = "tp"                      # set switching scheme
 swtype    = "none"                    # for frequency switching
 swper     = 1.0                       # one second cycle for switching
 swfreq    = 0.0, 0.0                  # for freq switching
-tint      = 5                         # integration time (5s -> 0.1 beams)
+tint      = 0.5                       # integration time (5s -> 0.1 beams)
 vlow      = 0
 vhigh     = 0
 vframe    = "lsrk"                    # LSR - kinematic is the "normal" definition (don't use dynamic)
@@ -41,6 +17,32 @@ vdef      = "Radio"                   # radio (optical is also acceptable, but n
 noisecal  = "lo"
 pol       = "Circular"
 nchan     = "high"
-spect.levels = 9                      # nine level sampling
 #iftarget = 0.25                       # IF target is now always 1
-
+vegas.vpol='self'
+restfreq  = [ 
+{"restfreq": 4.05489,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.745365,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 6.480375,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.16021,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.82966,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 6.66847,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.269205,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.875375,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 6.67774,   'bandwidth': 23.44, "res":5.7, "delatfreq": 0},
+{"restfreq": 4.382045,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 5.00532,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 6.883205,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.3888,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 5.34614,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 7.09718,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.4989,    'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 6.09607,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 7.32012,   'bandwidth': 23.44, "res":5.7, "delatfreq": 0},
+{"restfreq": 4.59309,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.80,      'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.85,      'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 7.552495,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.61994,   'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 7.794815,  'bandwidth': 23.44, "res":5.7, "deltafreq": 0},
+{"restfreq": 4.4,       'bandwidth': 850.,  "res":61.0, "deltafreq": 0},
+]
